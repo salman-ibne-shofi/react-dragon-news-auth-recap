df9b466dd9a6546ec0693fd02a6c75c900186ebb
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-21 05:00:45 +0600

    create private route and do not redirect on reload

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,11 +1,27 @@
-
+import { useParams } from "react-router-dom";
+import Header from "../Shared/Header/Header";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const News = () => {
-    return (
-        <div>
-            <h2>news details</h2>
-        </div>
-    );
+	const { id } = useParams();
+
+	return (
+		<div>
+			<Header></Header>
+            <Navbar></Navbar>
+			<div className="grid md:grid-cols-4">
+				<div className="col-span-3">
+					<h2 className="text-5xl">News Detail</h2>
+					<p>{id}</p>
+				</div>
+				<div>
+					<RightSideNav></RightSideNav>
+				</div>
+			</div>
+			<h2>news details</h2>
+		</div>
+	);
 };
 
 export default News;
