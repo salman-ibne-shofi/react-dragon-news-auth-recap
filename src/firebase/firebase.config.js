8e0b01f1223111b908786b0ff1421b08e58787d1
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-22 04:05:14 +0600

    update firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCznGGtqRMbDEPDjID7ptPdiMPPSrkw3mY",
-  authDomain: "react-dragon-news-auth-a7ffa.firebaseapp.com",
-  projectId: "react-dragon-news-auth-a7ffa",
-  storageBucket: "react-dragon-news-auth-a7ffa.appspot.com",
-  messagingSenderId: "85875999023",
-  appId: "1:85875999023:web:0479b6d0815b529c7a61da"
+	apiKey: import.meta.env.VITE_APIKEY,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate, useLocation } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
 	const { user, loading } = useContext(AuthContext);
     const location = useLocation();
-    console.log(location);
+    console.log(location.pathname);
     
 
     if(loading){
@@ -15,7 +15,7 @@ const PrivateRoute = ({ children }) => {
 	if (user) {
 		return children;
 	}
-	return <Navigate to="/login"></Navigate>;
+	return <Navigate state={location.pathname} to="/login"></Navigate>;
 };
 
 export default PrivateRoute;
